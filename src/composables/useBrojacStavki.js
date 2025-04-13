b7671a52fc43588df35f5fc7f94c14de007e65b7
Antonio Labinjan <alabinjan6@gmail.com>
2025-04-14 00:33:23 +0200

    Update useBrojacStavki.js
    comments

--- src/composables/useBrojacStavki.js
+++ src/composables/useBrojacStavki.js
@@ -1,9 +1,13 @@
 import { ref } from 'vue'
 
+// definiraš si funkciju i definiraš da će se ona izvozit iz ovog fajla
 export default function useBrojacStavki() {
+  // inicijaliziramo brojač na 0
   const brojac = ref(0)
+  // postavljamo ga na vrijednost koju primi funkcija
   const postavi = (vrijednost) => {
     brojac.value = vrijednost
   }
+  // funkcija vraća brojač i metodu postavi
   return { brojac, postavi }
 }
