f2a9c72d904595830286d1c3ad523b67e983cd95
Antonio Labinjan <alabinjan6@gmail.com>
2025-04-14 00:34:47 +0200

    Update ToDoHeader.vue
    comments

--- src/components/ToDoHeader.vue
+++ src/components/ToDoHeader.vue
@@ -1,14 +1,18 @@
 <template>
     <header class="header">
+      <!-- dohvaća se slot iz app.vue i dinamički se dohvaća naslov i brojač-->
+
       <slot />
     </header>
   </template>
   
   <script setup>
-  // Možeš dodati bilo kakvu logiku ovdje ako je potrebno
+  // Možemo dodati bilo kakvu logiku ovdje ako je potrebno
   </script>
   
   <style scoped>
+
+  /* najte me za css-a nikaj pitati, jer vam to ne razmem */
   .header {
     font-size: 1.5rem; /* Za text-2xl */
     font-weight: bold; /* Za font-bold */
